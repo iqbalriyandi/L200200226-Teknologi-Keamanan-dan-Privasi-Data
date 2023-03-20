fde1147f9973d6e2a0efb85d6931bf14de29b18c
iqbalriyandi <107165624+iqbalriyandi@users.noreply.github.com>
2023-03-20 08:52:03 +0700

    tugas1

--- Tugas1/substitutiob_chiper.py
+++ Tugas1/substitutiob_chiper.py
@@ -18,33 +18,5 @@ def substitution_cipher(key, plaintext):
 
     return ciphertext
 
-ciphertext = substitution_cipher("phqgiumeaylnofdxjkrcvstzwb", "iqbal")
-print(ciphertext)
- 24  
-Tugas1/transposition_chiper.py
-Comment on this file
-@@ -0,0 +1,24 @@
-def transposition_cipher(key, plaintext):
-    # inisialisasi variabel
-    num_columns = len(key)
-    num_rows = -(-len(plaintext) // num_columns)
-    matrix = [''] * num_columns
-
-    for col in range(num_columns):
-        for row in range(num_rows):
-            index = col + row * num_columns
-            if index < len(plaintext):
-                matrix[col] += plaintext[index]
-            else:
-                matrix[col] += " "
-
-    ordered_matrix = [""] * num_columns
-    for i, char in enumerate(key):
-        index = ord(char) - ord('0') - 1
-        ordered_matrix[index] = matrix[i]
-    ciphertext = "".join(ordered_matrix)
-
-    return ciphertext
-
-ciphertext = transposition_cipher("3142", "iqbal")
+ciphertext = substitution_cipher("phqgiumeaylnofdxjkrcvstzwb", "gandhi")
 print(ciphertext)
